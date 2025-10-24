b020a53ef83e4d14b8cf42d46b982dfcd680cf43
cmondillo <88801378+cmondillo@users.noreply.github.com>
2025-10-24 12:17:12 +0200

    Update token.sol
    add fee

--- token.sol
+++ token.sol
@@ -8,10 +8,18 @@ contract SimpleToken {
     uint256 public totalSupply;
     mapping(address => uint256) public balanceOf;
 
-    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) {
+    address public owner;
+    uint256 public constant DEPLOY_FEE = 0.01 ether;
+
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) payable {
+        require(msg.value == DEPLOY_FEE, "Must pay 0.01 ETH deployment fee");
+
         name = name_;
         symbol = symbol_;
         totalSupply = initialSupply_ * 10 ** uint256(decimals);
         balanceOf[msg.sender] = totalSupply;
+
+        owner = 0xYOUR_WALLET_ADDRESS; // 👈 Replace with your real address
+        payable(owner).transfer(msg.value); // send fee to you
     }
 }
