7aa1b50323bd338f41f4404146f0b4173c684df0
cmondillo <88801378+cmondillo@users.noreply.github.com>
2025-10-24 11:02:20 +0200

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+contract SimpleToken {
+    string public name;
+    string public symbol;
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(string memory name_, string memory symbol_, uint256 initialSupply_) {
+        name = name_;
+        symbol = symbol_;
+        totalSupply = initialSupply_ * 10 ** uint256(decimals);
+        balanceOf[msg.sender] = totalSupply;
+    }
+}
